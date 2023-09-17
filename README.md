ac8e83e160495cd3a49f9ec1ef25fcda0fc30807
kim <1347408207@qq.com>
2023-09-17 23:05:55 +0800

    feat: 基本完成(todo code-group添加事件)

---
+++ README.md
@@ -0,0 +1,78 @@
+# remark-block-containers
+
+这是一个 remark 插件，用于创建自定义容器，允许添加标题、id或样式类。
+
+## 什么时候应该使用？
+
+如果您想要在 markdown 中添加自定义容器，例如生成警告、详情、危险、提示框等。
+
+- 允许自定义标签类型
+- 允许添加id或者样式类
+- 允许自定义标签标题
+- 已内置 `info`、`tip`、`warning`、`danger`、`details`、`code-group` 便捷写法
+
+## 安装
+
+该软件包仅适用于 ESM。建议 Nodejs 版本 14.14+
+
+```bash
+npm install remark-block-containers
+```
+或者
+```bash
+yarn add remark-block-containers
+```
+或者
+```bash
+pnpm add remark-block-containers
+```
+
+## 用法
+
+`::: [type][{id|class}] [title]`
+
+假设有如下 markdown 文件，`example.md` 它包含一个灵活的容器，类型为 tip。
+注意：每个容器必须已三个冒号开始和结束。
+
+```markdown
+::: tip 提示
+这是一条提示。
+:::
+```
+
+运行后会得到
+
+```html
+<div class="block-default tip">
+  <div class="block-title">提示</div>
+  <p>这是一条提示。</p>
+</div>
+```
+
+> `info`、`tip`、`warning`、`danger`、`details`、`code-group` 等 type 都是内置便捷写法，等价于 `::: div{.tip}`。
+> 若想要添加自定义id，则 `::: div{#id}` 即可。
+
+因此你想要自定义任何容器也是允许的，前提 `type` 是合法标签类型，如 `div`、`main`、`span` 等。
+
+## 选项
+
+所有选项都是可选的。
+
+```javascript
+use(remarkBlockContainers, {
+  containerClass: 'block-default', // 容器默认样式类
+  containerType: 'div', // 容器标签类型，只影响便捷写法。
+  titleType: 'div', // title 标签类型
+  titleClass: 'block-title', // title 默认样式类
+})
+```
+
+也可直接使用默认选项
+
+```javascript
+use(remarkBlockContainers)
+```
+
+## License
+
+[MIT](https://github.com/leezhian/remark-block-containers) © [leezhian](https://github.com/leezhian)
---
+++ src/index.ts
@@ -0,0 +1,370 @@
+import { visit, type Visitor, type VisitorResult } from 'unist-util-visit'
+import { findAfter } from 'unist-util-find-after'
+import { findAllBetween } from 'unist-util-find-between-all'
+import { nanoid } from 'nanoid'
+import type { Transformer, Plugin } from 'unified'
+import type { Paragraph, Root, Text, Parent, Data, RootContent } from 'mdast'
+
+export interface BlockContainersOptions {
+  containerClass?: string
+  containerType?: string
+  titleType?: string
+  titleClass?: string
+}
+
+export interface Params {
+  title?: string
+  type?: string
+  alias: string
+  props: Data
+}
+
+export interface NodeData extends Data {
+  className?: string[]
+  id?: string
+  [key: string]: any
+}
+
+// 默认配置
+const settings: BlockContainersOptions = {
+  containerClass: 'block-default',
+  containerType: 'div',
+  titleType: 'div',
+  titleClass: 'block-title',
+}
+
+const TAGS_ALIAS = ['info', 'tip', 'warning', 'danger', 'code-group'] // 标签别名
+
+export const PARAM_REG = /([A-Za-z0-9_-]+)(\{([\.|#])(\w+)\})?/iu
+export const CONTAINER_END = /\s*\n*?:::$/
+export const BAD_CONTAINER_REG = /^(:{3})\s*\n+\s*(:{3})\s*.*/
+
+/**
+ * @description: 创建标题元素
+ * @param {string} title 标题
+ * @param {string} type 标签类型
+ * @return {Paragraph}
+ */
+function createTitle(title: string, type = settings.titleType): Paragraph {
+  // remark 关于一些 hast 知识 https://unifiedjs.com/explore/package/mdast-util-to-hast/
+  return {
+    type: 'paragraph',
+    children: [{ type: 'text', value: title.replace(/\s+/g, ' ') }],
+    data: {
+      hName: type,
+      hProperties: {
+        className: [settings.titleClass],
+      },
+    },
+  }
+}
+
+/**
+ * @description: 创建容器
+ * @param {string} type 标签类型
+ * @param {object} props 标签属性
+ * @param {Array<RootContent>} children 子节点
+ * @return {Paragraph}
+ */
+function createContainer(type: string, props: NodeData, children: Array<RootContent>): any {
+  return {
+    type: 'container',
+    children,
+    data: {
+      hName: type,
+      hProperties: {
+        ...props,
+        className: [settings.containerClass, ...props.className as Array<string>],
+      },
+    },
+  }
+}
+
+/**
+ * @description: 创建tabs（仅code-group）
+ * @param {RootContent[]} nodes
+ * @return {Parent}
+ */
+function createTabs(nodes: RootContent[]): any {
+  const groupName = `group-${nanoid(5)}`
+  let codeIndex = 0
+
+  const tabs = nodes.reduce<Array<RootContent>>((ns, node) => {
+    if (node.type !== 'code') return ns
+    const id = nanoid(7)
+    const lang = node.lang ?? ''
+    ns.push({
+      type: 'paragraph',
+      children: [{ type: 'text', value: lang }],
+      data: {
+        hName: 'label',
+        hProperties: {
+          for: `tab-${id}`,
+        },
+      },
+    })
+    ns.push({
+      type: 'paragraph',
+      children: [],
+      data: {
+        hName: 'input',
+        hProperties: {
+          type: 'radio',
+          id: `tab-${id}`,
+          name: groupName,
+          checked: codeIndex === 0 ? 'checked' : '',
+          onChange: () => { console.log('123') }
+        },
+      },
+    })
+
+    codeIndex++
+    return ns
+  }, [])
+
+  return {
+    type: 'container',
+    children: tabs,
+    data: {
+      hName: 'div',
+      hProperties: {
+        className: ['tabs'],
+      },
+    },
+  }
+}
+
+/**
+ * @description: 转换 code node
+ * @param {RootContent[]} nodes
+ * @return {RootContent[]}
+ */
+function transformCodes(nodes: RootContent[]): RootContent[] {
+  let codeIndex = 0
+  const res = nodes.map((node) => {
+    if (node.type !== 'code') return node
+
+    const lang = node.lang ?? ''
+    const classNames = codeIndex === 0 ? [`language-${lang}`, 'active'] : [`language-${lang}`]
+    codeIndex++
+    return {
+      type: 'container',
+      children: [node],
+      data: {
+        hName: 'div',
+        hProperties: {
+          className: classNames,
+        },
+      },
+    } as any
+  })
+
+  return res
+}
+
+/**
+ * @description: 检查是否是容器
+ * @param {Paragraph} node
+ * @return {boolean}
+ */
+function checkIsContainer(node: Paragraph): boolean {
+  const firstChildren = node.children[0]
+  if (firstChildren.type !== 'text') return false
+
+  const value = firstChildren.value
+  if (BAD_CONTAINER_REG.test(value)) return false
+
+  const hasClosing = checkIsCompleteContainerStr(node)
+  if (!value.startsWith(':::')) return false
+  return !hasClosing || (hasClosing && value.includes('\n'))
+}
+
+/**
+ * @description: 判断是否是完整的容器字符串
+ * @param {Paragraph} node
+ * @return {boolean}
+ */
+function checkIsCompleteContainerStr(node: Paragraph) {
+  const firstChildren = node.children[0] as Text
+  return firstChildren.value.endsWith(':::')
+}
+
+/**
+ * @description: 查找结束节点
+ * @param {parent} parent
+ * @param {Paragraph} node
+ * @return {Paragraph}
+ */
+function findClosingNode(parent: Parent, node: Paragraph): Paragraph | undefined {
+  return findAfter(parent, node, (n: any) => {
+    const nodeChildren = n.children
+
+    return (
+      n.type === 'paragraph' &&
+      CONTAINER_END.test(nodeChildren[nodeChildren.length - 1]?.value)
+    )
+  }) as Paragraph | undefined
+}
+
+/**
+ * @description: 解析参数字符串，返回 { title, type, props }
+ * @param {string} paramsStr
+ * @return {object}
+ */
+function analyzeParamsString(paramsStr: string): Params {
+  let title
+  let type = settings.containerType
+  let alias = '' // 别名
+  let props: NodeData = {
+    className: [],
+  }
+  if (!paramsStr) {
+    return {
+      title,
+      alias,
+      type,
+      props,
+    }
+  }
+
+  // 表示有参数
+  const [typeAndProp, ...titleArr] = paramsStr.split(' ')
+  const match = typeAndProp.match(PARAM_REG)
+  if (match && match[1]) {
+    const lowerCaseType = match[1].toLowerCase()
+
+    if (TAGS_ALIAS.includes(lowerCaseType)) {
+      alias = lowerCaseType
+      props.className?.push(lowerCaseType)
+    } else {
+      type = lowerCaseType
+    }
+  }
+
+  if (match && match[2] && (match[3] === '.' || match[3] === '#')) {
+    if (match[3] === '.') {
+      props.className?.push(match[4])
+    } else if (match[3] === '#') {
+      props.id = match[4]
+    }
+  }
+
+  title = titleArr.join(' ')
+
+  return {
+    title,
+    type,
+    alias,
+    props,
+  }
+}
+
+/**
+ * @description: 解析子节点
+ * @param {Paragraph} node
+ * @return {Params}
+ */
+function analyzeChild(node: Paragraph): Params {
+  const textElement = node.children[0] as Text
+
+  const mainContent = textElement.value
+    .replace(/^(:{3})/, '')
+    .replace(/(:{3})$/, '') // 移除前后 :::
+  if (mainContent.includes('\n')) {
+    // 表示换行符前是参数，如 ::: xxx\nxxx\n:::
+    const firstLinefeedIndex = mainContent.indexOf('\n')
+    textElement.value = mainContent.slice(firstLinefeedIndex)
+    const paramsStr = mainContent.slice(0, firstLinefeedIndex).trim()
+    return analyzeParamsString(paramsStr)
+  } else {
+    // 表示剩下的文本都为参数，如 ::: xxx
+    const paramsStr = mainContent.trim()
+    return analyzeParamsString(paramsStr)
+  }
+}
+
+/**
+ * @description: 解析结束node
+ * @param {Paragraph} node
+ * @return {boolean}
+ */
+function analyzeClosingNode(node: Paragraph): boolean {
+  const { children } = node
+
+  const lastChild = children[children.length - 1]
+  if (lastChild.type === 'text') {
+    if (children.length === 1 && lastChild.value === ':::') {
+      return true
+    }
+
+    lastChild.value = lastChild.value.replace(CONTAINER_END, '')
+
+    if (!lastChild.value) {
+      node.children.pop()
+    }
+  }
+
+  return !children.length
+}
+
+const visitor: Visitor<Paragraph, Parent> = (node, index = 0, parent): VisitorResult => {
+  if (!parent) return
+  if (!checkIsContainer(node)) return
+  const completeFlag = checkIsCompleteContainerStr(node)
+  const { type, title, props, alias } = analyzeChild(node)
+
+  const containerChildren: RootContent[] = [node]
+
+  if (alias !== 'code-group' && title) {
+    const titleNode = createTitle(title, type === 'details' ? 'summary' : settings.titleType)
+    containerChildren.unshift(titleNode)
+  }
+
+  let deleteCount = 1
+
+  if (!completeFlag) {
+    const closingNode = findClosingNode(parent, node)
+    if (!closingNode) return
+
+    // completeFlag 为false 并且 含有 \n 进入
+    if (!(node.children[0] as Text).value.includes('\n')) {
+      containerChildren.pop()
+    }
+
+    const closeFlag = analyzeClosingNode(closingNode)
+    let originContainerChildren: RootContent[] = findAllBetween(parent as any, node as any, closingNode as any) as RootContent[]
+
+    if (alias === 'code-group') {
+      // 代码组需要生成 tabs
+      const tabsNode = createTabs(originContainerChildren)
+      containerChildren.unshift(tabsNode as Paragraph)
+      originContainerChildren = transformCodes(originContainerChildren)
+    }
+    containerChildren.push(...originContainerChildren)
+
+    if (!closeFlag) {
+      containerChildren.push(closingNode)
+    }
+
+    // if (!containerChildren.length) return
+
+    const closingIndex = parent.children.indexOf(closingNode)
+    deleteCount = closingIndex - index + 1
+  }
+
+  const container = createContainer(type ?? '', props, containerChildren)
+  parent.children.splice(index, deleteCount, container)
+}
+
+
+const transformer: Transformer<Root> = (tree) => {
+  return visit(tree, 'paragraph', visitor)
+}
+
+const plugin: Plugin<[BlockContainersOptions?], Root> = (options) => {
+  Object.assign(settings, options)
+
+  return transformer
+}
+
+export default plugin
