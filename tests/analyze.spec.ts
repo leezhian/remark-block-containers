39b7f49d4b37c35a67ec8d35f9905b10fdae7253
kim <1347408207@qq.com>
2024-02-07 09:32:09 +0800

    test: 修改测试

--- tests/analyze.spec.ts
+++ tests/analyze.spec.ts
@@ -1,124 +1,44 @@
-import { mockNodeChild } from './test-case.mock'
-import { BAD_CONTAINER_REG, CONTAINER_START, CONTAINER_END, PARAM_REG, type Params, type NodeData } from '../src'
+import { unified } from 'unified'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import rehypeStringify from 'rehype-stringify'
+import remarkBlockContainers from '../src'
+import { mockMd, mockMdByOptions } from './test-case.mock'
 
+const defaultCompiler = unified()
+  .use(remarkParse)
+  .use(remarkBlockContainers)
+  .use(remarkRehype)
+  .use(rehypeStringify)
 
-interface NodeChild {
-  type: string
-  value?: string
-}
+const optionCompiler = unified()
+  .use(remarkParse)
+  .use(remarkBlockContainers, {
+    titleClass: 'custom-title',
+    titleType: 'div',
+    containerClass: 'custom-container',
+    containerType: 'section'
+  })
+  .use(remarkRehype)
+  .use(rehypeStringify)
 
-const TAGS_ALIAS = ['info', 'tip', 'warning', 'danger', 'code-group']
+const process = async (compiler: any, contents: string): Promise<string> => {
+  const file = await compiler.process(contents)
+  return String(file.value).replace('\n', '')
+};
 
-function mockCheckIsCompleteContainerStr(node: NodeChild) {
-  const firstChildren = node
-  const clolonCount = (firstChildren.value ?? '').match(/:/g)?.length ?? 0
-  return CONTAINER_END.test(firstChildren.value ?? '') && clolonCount >= 6
-}
-
-const mockCheckIsContainer = (node: NodeChild) => {
-  const firstChildren = node
-  if (firstChildren.type !== 'text') return false
-
-  const value = firstChildren.value!
-  if (BAD_CONTAINER_REG.test(value)) return false
-
-  const hasClosing = mockCheckIsCompleteContainerStr(node)
-  if (hasClosing && CONTAINER_START.test(value)) {
-    return value.includes('\n');
-  }
-  else {
-    return hasClosing || CONTAINER_START.test(value);
-  }
-}
-
-function mockAnalyzeParamsString(paramsStr: string): Params {
-  let title: string | undefined = undefined
-  let type = 'div'
-  let alias = '' // 别名
-  const props: NodeData = {
-    className: [],
-  }
-  if (!paramsStr) {
-    return {
-      title,
-      alias,
-      type,
-      props,
-    }
-  }
-
-  // 表示有参数
-  const params = paramsStr.split(' ')
-  const typeAndProp = params.shift() ?? ''
-  const match = typeAndProp.match(PARAM_REG)
-  if (match && match[1]) {
-    const lowerCaseType = match[1].toLowerCase()
-
-    if (TAGS_ALIAS.includes(lowerCaseType)) {
-      alias = lowerCaseType
-      props.className?.push(lowerCaseType)
-    } else {
-      type = lowerCaseType
-    }
-  }
-
-  if (match && match[2] && (match[3] === '.' || match[3] === '#')) {
-    if (match[3] === '.') {
-      props.className?.push(match[4])
-    } else if (match[3] === '#') {
-      props.id = match[4]
-    }
-  }
-
-  if(params.length) {
-    title = params.join(' ')
-  }
-
-  return {
-    title,
-    type,
-    alias,
-    props,
-  }
-}
-
-/**
- * @description: 解析子节点
- * @param {Paragraph} node
- * @return {Params}
- */
-function mockAnalyzeChild(node: NodeChild): Params {
-  // 因为在正式代码中有前置判断，肯定是 text 元素
-  const mainContent = node.value!
-    .replace(/^(:{3})/, '')
-    .replace(/(:{3})$/, '') // 移除前后 :::
-  if (mainContent.includes('\n')) {
-    // 表示换行符前是参数，如 ::: xxx\nxxx\n:::
-    const firstLinefeedIndex = mainContent.indexOf('\n')
-    const paramsStr = mainContent.slice(0, firstLinefeedIndex).trim()
-    return mockAnalyzeParamsString(paramsStr)
-  } else {
-    // 表示剩下的文本都为参数，如 ::: xxx
-    const paramsStr = mainContent.trim()
-    return mockAnalyzeParamsString(paramsStr)
-  }
-}
-
-describe('checkIsContainer test', () => {
-  mockNodeChild.forEach(item => {
-    const node = item.input
-    test(`checkIsContainer type: ${node.type} value: ${node.value}`, () => {
-      expect(mockCheckIsContainer(node)).toBe(item.output)
+describe('analyze', () => {
+  mockMd.forEach(item => {
+    test(`test input ${item.input}`, async () => {
+      expect(await process(defaultCompiler, item.input)).toMatchInlineSnapshot(item.output,);
     })
   })
 })
 
-describe('analyzeParamsString test', () => {
-  mockNodeChild.forEach(item => {
-    if (!item.output) return
-    const node = item.input
-    test(`analyzeParamsString type: ${node.type} value: ${node.value}`, () => {
-      expect(mockAnalyzeChild(node)).toEqual(item.paramsOutput)
+describe('test options', () => {
+  mockMdByOptions.forEach(item => {
+      test(`test options input ${item.input}`, async () => {
+      expect(await process(optionCompiler, item.input)).toMatchInlineSnapshot(item.output,);
     })
   })
 })
--- tests/test-case.mock.ts
+++ tests/test-case.mock.ts
@@ -1,255 +1,216 @@
-export const mockNodeChild = [
+import dedent from 'dedent'
+
+export const mockMd = [
   {
-    input: {
-      type: 'code',
-      value: '```js```'
-    },
-    output: false,
-    paramsOutput: null
-  }, {
-    input: {
-      type: 'code',
-      value: ':::'
-    },
-    output: false,
-    paramsOutput: null
+    input: dedent
+    `
+    :::
+    :::
+    `,
+    output: `"<p>::::::</p>"`
   },
   {
-    input: {
-      type: 'text',
-      value: ':::'
-    },
-    output: true,
-    paramsOutput: {
-      title: undefined,
-      alias: '',
-      type: 'div',
-      props: {
-        className: []
-      },
-    }
+    input: dedent
+    `
+    :::
+
+    :::
+    `,
+    output: `"<div class="block-default"></div>"`
   },
   {
-    input: {
-      type: 'text',
-      value: '::: :::'
-    },
-    output: false,
-    paramsOutput: null
+    input: dedent
+    `
+    :::
+    content
+    :::
+    `,
+    output: `"<div class="block-default"><p>content</p></div>"`
   },
   {
-    input: {
-      type: 'text',
-      value: '::: \n :::'
-    },
-    output: false,
-    paramsOutput: null
+    input: dedent
+    `
+    :::info
+    content
+    :::
+    `,
+    output: `"<div class="block-default info"><p>content</p></div>"`
   },
   {
-    input: {
-      type: 'text',
-      value: ':::tip'
-    },
-    output: true,
-    paramsOutput: {
-      title: undefined,
-      alias: 'tip',
-      type: 'div',
-      props: {
-        className: ['tip']
-      },
-    }
+    input: dedent
+    `
+    :::tip
+    content
+    :::
+    `,
+    output: `"<div class="block-default tip"><p>content</p></div>"`
   },
   {
-    input: {
-      type: 'text',
-      value: '::: tip'
-    },
-    output: true,
-    paramsOutput: {
-      title: undefined,
-      alias: 'tip',
-      type: 'div',
-      props: {
-        className: ['tip']
-      },
-    }
+    input: dedent
+    `
+    :::warning
+    content
+    :::
+    `,
+    output: `"<div class="block-default warning"><p>content</p></div>"`
   },
   {
-    input: {
-      type: 'text',
-      value: '::: tip{id}'
-    },
-    output: true,
-    paramsOutput: {
-      title: undefined,
-      alias: 'tip',
-      type: 'div',
-      props: {
-        className: ['tip']
-      },
-    }
+    input: dedent
+    `
+    :::danger
+    content
+    :::
+    `,
+    output: `"<div class="block-default danger"><p>content</p></div>"`
   },
   {
-    input: {
-      type: 'text',
-      value: '::: tip{#id}'
-    },
-    output: true,
-    paramsOutput: {
-      title: undefined,
-      alias: 'tip',
-      type: 'div',
-      props: {
-        className: ['tip'],
-        id: 'id'
-      },
-    }
+    input: dedent
+    `
+    :::details
+    content
+    :::
+    `,
+    output: `"<details class="block-default details"><p>content</p></details>"`
   },
   {
-    input: {
-      type: 'text',
-      value: '::: tip{.class}'
-    },
-    output: true,
-    paramsOutput: {
-      title: undefined,
-      alias: 'tip',
-      type: 'div',
-      props: {
-        className: ['tip', 'class']
-      },
-    }
+    input: dedent
+    `
+    :::details 详情
+    content
+    :::
+    `,
+    output: `"<details class="block-default details"><summary class="block-title">详情</summary><p>content</p></details>"`
   },
   {
-    input: {
-      type: 'text',
-      value: '::: tip提示'
-    },
-    output: true,
-    paramsOutput: {
-      title: undefined,
-      alias: 'tip',
-      type: 'div',
-      props: {
-        className: ['tip']
-      },
-    }
+    input: dedent
+    `
+    ::: tip
+    content
+    :::
+    `,
+    output: `"<div class="block-default tip"><p>content</p></div>"`
   },
   {
-    input: {
-      type: 'text',
-      value: '::: tip 提示'
-    },
-    output: true,
-    paramsOutput: {
-      title: '提示',
-      alias: 'tip',
-      type: 'div',
-      props: {
-        className: ['tip']
-      },
-    }
+    input: dedent
+    `
+    ::: tip{.wrap}
+    content
+    :::
+    `,
+    output: `"<div class="block-default tip wrap"><p>content</p></div>"`
   },
   {
-    input: {
-      type: 'text',
-      value: '::: tip{id} 提示'
-    },
-    output: true,
-    paramsOutput: {
-      title: '提示',
-      alias: 'tip',
-      type: 'div',
-      props: {
-        className: ['tip']
-      },
-    }
+    input: dedent
+    `
+    ::: tip{#box}
+    content
+    :::
+    `,
+    output: `"<div class="block-default tip" id="box"><p>content</p></div>"`
   },
   {
-    input: {
-      type: 'text',
-      value: '::: tip{#id} 提示'
-    },
-    output: true,
-    paramsOutput: {
-      title: '提示',
-      alias: 'tip',
-      type: 'div',
-      props: {
-        className: ['tip'],
-        id: 'id'
-      },
-    }
+    input: dedent
+    `
+    ::: tip 提示
+    content
+    :::
+    `,
+    output: `"<div class="block-default tip"><p class="block-title">提示</p><p>content</p></div>"`
   },
   {
-    input: {
-      type: 'text',
-      value: '::: tip{#id} 提示 :::'
-    },
-    output: false,
-    paramsOutput: null
+    input: dedent
+    `
+    ::: tip{.wrap} 提示
+    content
+    :::
+    `,
+    output: `"<div class="block-default tip wrap"><p class="block-title">提示</p><p>content</p></div>"`
   },
   {
-    input: {
-      type: 'text',
-      value: '::: tip{#id} 提示 \n:::'
-    },
-    output: true,
-    paramsOutput: {
-      title: '提示',
-      alias: 'tip',
-      type: 'div',
-      props: {
-        className: ['tip'],
-        id: 'id'
-      },
-    }
+    input: dedent
+    `
+    ::: main
+    content
+    :::
+    `,
+    output: `"<main class="block-default"><p>content</p></main>"`
   },
   {
-    input: {
-      type: 'text',
-      value: '::: tip{#id} 提示 \n123:::'
-    },
-    output: true,
-    paramsOutput: {
-      title: '提示',
-      alias: 'tip',
-      type: 'div',
-      props: {
-        className: ['tip'],
-        id: 'id'
-      },
-    }
+    input: dedent
+    `
+    ::: main{.wrap}
+    content
+    :::
+    `,
+    output: `"<main class="block-default wrap"><p>content</p></main>"`
   },
   {
-    input: {
-      type: 'text',
-      value: '::: div 提示'
-    },
-    output: true,
-    paramsOutput: {
-      title: '提示',
-      alias: '',
-      type: 'div',
-      props: {
-        className: []
-      },
-    }
+    input: dedent
+    `
+    ::: main{.wrap} 标题
+    content
+    :::
+    `,
+    output: `"<main class="block-default wrap"><p class="block-title">标题</p><p>content</p></main>"`
   },
   {
-    input: {
-      type: 'text',
-      value: '::: code-group 代码组'
-    },
-    output: true,
-    paramsOutput: {
-      title: '代码组',
-      alias: 'code-group',
-      type: 'div',
-      props: {
-        className: ['code-group']
-      },
-    }
+    input: dedent
+    `
+    ::: tip
+
+    content
+
+    :::
+    `,
+    output: `"<div class="block-default tip"><p>content</p></div>"`
+  },
+  {
+    input: dedent
+    `
+    ::: tip
+    content
+
+    :::
+    `,
+    output: `"<div class="block-default tip"><p>content</p></div>"`
+  },
+  {
+    input: dedent
+    `
+    ::: tip
+    content \`npm\`
+    :::
+    `,
+    output: `"<div class="block-default tip"><p>content <code>npm</code></p></div>"`
+  }
+]
+
+export const mockMdByOptions = [
+  {
+    input: dedent
+    `
+    :::tip
+    content
+    :::
+    `,
+    output: `"<section class="custom-container tip"><p>content</p></section>"`
+  },
+  {
+    input: dedent
+    `
+    :::tip 提示
+    content
+    :::
+    `,
+    output: `"<section class="custom-container tip"><div class="custom-title">提示</div><p>content</p></section>"`
+  },
+  {
+    input: dedent
+    `
+    :::tip{.wrap} 提示
+    content
+    :::
+    `,
+    output: `"<section class="custom-container tip wrap"><div class="custom-title">提示</div><p>content</p></section>"`
   },
 ]
